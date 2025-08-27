06613b3b87d7db853c7a78afe028dfc520623a05
Clara <whyzaynan@gmail.com>
2025-08-27 17:56:51 +0200

    Proyecto inicial Spring Boot

---
+++ src/main/java/com/example/demoartifac/DemoartifacApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoartifac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoartifacApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(DemoartifacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoartifac/DemoartifacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoartifac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoartifacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
